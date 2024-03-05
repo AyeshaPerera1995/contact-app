654a059f2f90a03b4a85a74464f99a91aedbfa18
Ayesha Perera <aypdev1995@gmail.com>
2024-03-05 19:38:42 +0000

    create basic headers and other in main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 import React from 'react';
+import Header from './Header';
+import AddContact from './AddContact'
+import ContactList from './ContactList'
 
 function App() {
+const contacts = [
+  {
+    id: "1",
+    name: "Ayesha",
+    email: "ayp@gmail.com"
+  },
+  {
+    id: "2",
+    name: "Nigethan",
+    email: "nik@gmail.com"
+  },
+]
+
   return (
-    <div>Hello World</div>
+    <div className='ui container'>
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts}/>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />   {/* call App.js file components */}
   </React.StrictMode>
 );
